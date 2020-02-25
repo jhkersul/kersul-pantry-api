7c22d5fbf4ca15468313318d3f562ac6d1fb645f
Kersul <joaohkfaria@gmail.com>
2020-02-25 12:18:33 -0300

    Adding docs to Joi transform

--- src/adapters/validations/JoiTransform.js
+++ src/adapters/validations/JoiTransform.js
@@ -1,3 +1,8 @@
+/**
+ * Transform Joi error to error details format
+ * @param {Object} error Joi error object
+ * @returns {Array} Error details. Ex: [{ target: 'abc', message: 'cde' }]
+ */
 export function transformJoiToErrorDetails(error) {
   return error.details.map((errorDetail) => ({
     target: errorDetail.path.join('.'),
